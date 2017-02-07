7141c4a68f2113710c5d061ceda7ad249ee6210c
chakpongchung <chakpongchung@gmail.com>
2017-02-07 11:16:18 -0500

    update

--- code/GiveUsA/updateinjection.js
+++ code/GiveUsA/updateinjection.js
@@ -2,21 +2,22 @@
 
 scriptForm = null
 
-setTimeout(function(){
-    scriptForm = document.getELementById("veryUniqueIdToModifyInjectedScriptForm");
+    setTimeout(function() {
+      scriptForm =
+          document.getELementById("veryUniqueIdToModifyInjectedScriptForm");
 
-    if(scriptForm !== null){
-        if(scriptForm.addEventListener){
-            scriptForm.addEventListener("submit", callback, false);
-        } else if(scriptForm.attachEvent){
-            scriptForm.attachEvent("onsubmit", callback);
+      if (scriptForm !== null) {
+        if (scriptForm.addEventListener) {
+          scriptForm.addEventListener("submit", callback, false);
+        } else if (scriptForm.attachEvent) {
+          scriptForm.attachEvent("onsubmit", callback);
         }
-    }
+      }
 
-    console.log(scriptForm);
-}, 10);
+      console.log(scriptForm);
+    }, 10);
 
-function callback(){
-    script = this.element[0].value;
-    chrome.runtime.sendMessage({new_injected_script : script});
+function callback() {
+  script = this.element[0].value;
+  chrome.runtime.sendMessage({new_injected_script: script});
 };
